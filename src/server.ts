70840309fe7a5efeed6b4a551c2422829862ef5f
MDSanju <mdsanju387@gmail.com>
2023-07-24 23:13:07 -0700

    [DONE] Setup logger successfully

--- src/server.ts
+++ src/server.ts
@@ -2,19 +2,20 @@ import mongoose from 'mongoose'
 
 import app from './app'
 import config from './config'
+import { errorLogger, infoLogger } from './shared/logger'
 
 // Database connection
 async function main() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log('Successfully database connected!')
+    infoLogger.info('Successfully database connected!')
 
     // Start the server
     app.listen(config.port, () => {
-      console.log(`Server is running on port ${config.port}`)
+      infoLogger.info(`Server is running on port ${config.port}`)
     })
   } catch (error) {
-    console.log('Failed to connect database', error)
+    errorLogger.error('Failed to connect database', error)
   }
 }
 
